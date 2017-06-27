c17d23a507789299515668215bc5dc9127effd30
mwatha <mwathabwanali@gmail.com>
2017-06-27 15:19:34 +0200

    Working on tables

---
+++ app/models/birth_registration_type.rb
@@ -0,0 +1,6 @@
+class BirthRegistrationType < ActiveRecord::Base
+  self.table_name   = :birth_registration_type
+  self.primary_key  = :birth_registration_type_id
+  include EbrsAttribute
+   
+end
