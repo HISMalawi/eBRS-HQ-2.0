010c1b39852c3e72d0757d0cd08435916dd6e2b3
mangochiman <mangochiman@gmail.com>
2017-06-28 14:49:21 +0200

    Generating couch models

--- app/models/location_couchdb.rb
+++ app/models/location_couchdb.rb
@@ -1,2 +1,33 @@
+require 'couchrest_model'
 class LocationCouchdb < CouchRest::Model::Base
+
+  property :location_id, Integer
+  property :code, String
+  property :name, String
+  property :description, String
+  property :postal_code, String
+  property :country, String
+  property :latitude, String
+  property :longitude, String
+  property :county_district, String
+  
+  property :creator, Integer
+  property :voided, TrueClass, :default => false
+  property :void_reason, String
+  property :voided_by, Integer
+  property :date_voided, Date
+  
+  timestamps!
+  
+  design do
+    view :by_location_id
+    view :by_code
+    view :by_name
+    view :by_description
+    view :by_postal_code
+    view :by_country
+    view :by_latitude
+    view :by_longitude
+    view :by_county_district
+  end
 end
