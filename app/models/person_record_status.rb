47bb87d998823559217edf11b93dd68c86566a83
mwatha <mwathabwanali@gmail.com>
2017-06-28 22:57:39 +0200

    making sure app runs in production mode

--- app/models/person_record_status.rb
+++ app/models/person_record_status.rb
@@ -3,6 +3,6 @@ class PersonRecordStatus < ActiveRecord::Base
     self.primary_key = :person_record_status_id
     include EbrsAttribute
 
-    belongs_to :person, :foreign_key: "person_id"
+    belongs_to :person, foreign_key: "person_id"
 
 end
