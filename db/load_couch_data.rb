3ad4273359a3b604e96d76553fbafac3885e5040
phundi <kennethkapundi1@gmail.com>
2017-07-27 11:08:41 +0200

    removed metadata from transport layer

--- db/load_couch_data.rb
+++ db/load_couch_data.rb
@@ -42,6 +42,7 @@ end
 
 protocol = $configs['secure_connection'].to_s == 'true' ? 'https' : 'http'
 last_seq = JSON.parse(RestClient.get("#{protocol}://#{$configs['host']}:#{$configs['port']}/#{$configs['prefix']}_#{$configs['suffix']}/_changes"))['last_seq']
-ActiveRecord::Base.connection.execute("UPDATE couch_sequence SET seq  = #{last_seq}")
+puts "Setting sequence to #{last_seq}"
+ActiveRecord::Base.connection.update("UPDATE couch_sequence SET seq  = #{last_seq}")
 
 puts "Done Loading Data to Couch!!"
