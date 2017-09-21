11dd8b3e99b6d60a71f59c64f7c9ae9199ad3bcb
comishOmar <comish2009x@live.com>
2017-09-21 16:37:14 +0200

    Added the pegination logic in the migration scripts

--- bin/test.rb
+++ bin/test.rb
@@ -185,5 +185,136 @@ def func
    
 end
 
+
+def kick_start
+
+	total_records = Child.count
+	page_size = 100
+	total_pages = (total_records / page_size) + (total_records % page_size)
+	current_page = 1
+
+	while (current_page < total_pages) do
+        puts "<<<<<<<<<<<<<<<< Page number: #{current_page}"
+        puts "\n"
+        test_method(current_page, page_size)
+		current_page = current_page + 1	
+	end
+end 
+
+def test_method(current_pge, pge_size)
+
+	data ={}
+=begin
+	data = {person:{duplicate:"", is_exact_duplicate:"", relationship:"normal", last_name:"Ndala",
+	        first_name:"Zimbota", middle_name:"", birthdate:"30/Oct/2015", birth_district:"Blantyre", 
+	        gender:"Male", place_of_birth:"Hospital", hospital_of_birth:"Mlambe Hospital", 
+	        birth_weight:"3.700", type_of_birth:"Single", parents_married_to_each_other:"Yes", 
+	        court_order_attached:"", parents_signed:"", national_serial_number:"00000214140", 
+	        district_id_number:"BT/0000125/2016", mother:{last_name:"Mkuwu", first_name:"Angella", 
+	        middle_name:"", birthdate:"22/Oct/1985", birthdate_estimated:"", 
+	        citizenship:"Malawian", residential_country:"Malawi", current_district:"Blantyre", 
+	        current_ta:"Machinjiri", current_village:"Machinjiri", home_district:"Mangochi", 
+	        home_ta:"Mponda", home_village:"Michesi"}, mode_of_delivery:"SVD", 
+	        level_of_education:"Secondary", father:{birthdate_estimated:"", 
+	        residential_country:"Malawi"}, informant:{last_name:"Mkuwu", first_name:"Angella", 
+	        middle_name:"", relationship_to_person:"Mother", current_district:"Blantyre", 
+	        current_ta:"Machinjiri", current_village:"Machinjiri", addressline1:"", 
+	        addressline2:"", phone_number:"0993853933"}, form_signed:"Yes", 
+	        acknowledgement_of_receipt_date:"2015-11-06 160456 -1000".to_date.strftime("%d/%b/%Y")}, 
+	        home_address_same_as_physical:"Yes", gestation_at_birth:"37", number_of_prenatal_visits:"4", 
+	        month_prenatal_care_started:"3", number_of_children_born_alive_inclusive:"2", 
+	        number_of_children_born_still_alive:"2", same_address_with_mother:"", 
+	        informant_same_as_mother:"Yes", registration_type:"normal", 
+	        record_status:"PRINTED", _rev:"7-213580c4bcfdf268c901d5c3b11617c2", 
+	        _id:"0275f9967f6069e00bbf2d310871a2f8", request_status:"DISPATCHED", 
+	        biological_parents:"", foster_parents:"", parents_details_available:"", 
+	        copy_mother_name:"No", controller:"person", action:"create"}
+=end
+  records = Child.all.page(current_pge).limit(pge_size)
+ 
+
+  (records || []).each do |r|
+     
+	  data = { person: {duplicate: "", is_exact_duplicate: "",
+					   relationship: r[:relationship],
+					   last_name: r[:last_name],
+					   first_name: r[:first_name],
+					   middle_name: r[:middle_name],
+					   birthdate: r[:birthdate],
+					   birth_district: r[:birth_district],
+					   gender: r[:gender],
+					   place_of_birth: r[:place_of_birth],
+					   hospital_of_birth: r[:hospital_of_birth],
+					   birth_weight: r[:birth_weight],
+					   type_of_birth: r[:type_of_birth],
+					   parents_married_to_each_other: r[:parents_married_to_each_other],
+					   court_order_attached: r[:court_order_attached],
+					   created_at: r[:created_at],
+					   created_by: r[:created_by],
+					   updated_at: r[:updated_at],
+					   parents_signed: "",
+					   national_serial_number: r[:national_serial_number],
+					   district_id_number: r[:district_id_number],
+					   mother:{
+					     last_name: r[:mother][:last_name],
+					     first_name: r[:mother][:first_name],
+					     middle_name: r[:mother][:middle_name],
+					     birthdate: r[:mother][:birthdate],
+					     birthdate_estimated: r[:mother][:birthdate_estimated],
+					     citizenship: r[:mother][:citizenship],
+					     residential_country: r[:mother][:residential_country],
+					     current_district: r[:mother][:current_district],
+					     current_ta: r[:mother][:current_ta],
+					     current_village: r[:mother][:current_village],
+					     home_district: r[:mother][:home_district],
+					     home_ta: r[:mother][:home_ta],
+					     home_village: r[:mother][:home_village]
+					  },
+					   mode_of_delivery: r[:mode_of_delivery],
+					   level_of_education: r[:level_of_education],
+					   father: {
+					     birthdate_estimated: r[:father][:birthdate_estimated],
+					     residential_country: r[:father][:residential_country]
+					  },
+					   informant: {
+					     last_name: r[:informant][:last_name],
+					     first_name: r[:informant][:first_name],
+					     middle_name: r[:informant][:middle_name],
+					     relationship_to_person: r[:informant][:relationship_to_child],
+					     current_district: r[:informant][:current_district],
+					     current_ta: r[:informant][:current_ta],
+					     current_village: r[:informant][:current_village],
+					     addressline1: r[:informant][:addressline1],
+					     addressline2: r[:informant][:addressline2],
+					     phone_number: r[:informant][:phone_number]
+					  },
+					   form_signed: r[:form_signed],
+					   acknowledgement_of_receipt_date: r[:acknowledgement_of_receipt_date]
+					  },
+					   home_address_same_as_physical: "Yes",
+					   gestation_at_birth: r[:gestation_at_birth],
+					   number_of_prenatal_visits: r[:number_of_prenatal_visits],
+					   month_prenatal_care_started: r[:month_prenatal_care_started],
+					   number_of_children_born_alive_inclusive: r[:number_of_children_born_alive_inclusive],
+					   number_of_children_born_still_alive: r[:number_of_children_born_still_alive],
+					   same_address_with_mother: "",
+					   informant_same_as_mother: (r[:informant][:relationship_to_child] == "Mother" ? "Yes" : "No"),
+					   registration_type: r[:relationship],
+					   record_status: r[:record_status],
+					   _rev: r[:_rev],
+					   _id: r[:_id],
+					   request_status: r[:request_status],
+					   biological_parents: "",
+					   foster_parents: "",
+					   parents_details_available: "",
+					   copy_mother_name: "No",
+					   controller: "person",
+					   action: "create"
+					  }
+
+			transform_record(data)
+   end
+end
+
 #func
 #get_prev_child_id
