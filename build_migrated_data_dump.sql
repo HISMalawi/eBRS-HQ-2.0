95bbe68dce4b0cf17fc8198404e2bc272a3b820a
phundi <kennethkapundi1@gmail.com>
2017-10-19 16:17:07 +0200

    testing migration

---
+++ build_migrated_data_dump.sql
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+ENV=$1
+
+echo "READING CONFIGURATIONS FOR METADATA"
+USERNAME=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['username']"`
+PASSWORD=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['password']"`
+DATABASE=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['database']"`
+HOST=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['host']"`
+
+echo $ENV
+echo 'BUILDING DATA WITHOUT SCHEMA'
+mysqldump -u $USERNAME -p$PASSWORD  --no-create-info --ignore-table=$DATABASE.sessions --ignore-table=$DATABASE.schema_migrations --ignore-table=$DATABASE.audit_trail_types --ignore-table=$DATABASE.birth_registration_type --ignore-table=$DATABASE.couchdb_sequence --ignore-table=$DATABASE.level_of_education  --ignore-table=$DATABASE.location --ignore-table=$DATABASE.location_tag --ignore-table=$DATABASE.location_tag_map --ignore-table=$DATABASE.mode_of_delivery --ignore-table=$DATABASE.person_attribute_types --ignore-table=$DATABASE.person_relationship_types --ignore-table=$DATABASE.person_identifier_types --ignore-table=$DATABASE.person_type_of_births --ignore-table=$DATABASE.person_type --ignore-table=$DATABASE.role --ignore-table=$DATABASE.statuses $DATABASE > bare_data.sql
+
+DIR=`pwd`
+echo "GENERATED DUMP FILE : $DIR/bare_data.sql"
+
