2b3093b896cc3e7208d21e91d19d501c88ff898e
phundi <kennethkapundi1@gmail.com>
2017-11-29 13:10:15 +0200

    Fix for loading uniq statuses

--- db/load_statuses.rb
+++ db/load_statuses.rb
@@ -3,9 +3,9 @@ names = []
 
 CSV.foreach("#{Rails.root}/app/assets/data/person_status.csv", :headers => true) do |row|
  next if row[3].blank?
- names << row[3].squish
  next if names.include?(row[3].squish)
-
+ names << row[3].squish
+ 
  status_type = Status.create!(name: row[3].squish, description: row[4].squish)
  puts "Loaded #{status_type.name}"
 end
