bbd8a8563b8dcb3e4030f236c35c2f3d453d159c
phundi <kennethkapundi1@gmail.com>
2017-12-01 14:30:03 +0200

    Removed @ and replaced with _

--- app/models/person_name.rb
+++ app/models/person_name.rb
@@ -11,15 +11,15 @@ class PersonName < ActiveRecord::Base
     before_create :check_length, :check_special_chars
 
   def check_length
-    self.first_name = '@@@@@' if (!self.first_name.blank? && self.first_name.length > 100 rescue true)
-    self.middle_name = '@@@@@' if (!self.middle_name.blank? && self.middle_name.length > 100 rescue true)
-    self.last_name = '@@@@@' if (!self.last_name.blank? && self.last_name.length > 100 rescue true)
+    self.first_name = '_' if (!self.first_name.blank? && self.first_name.length > 100 rescue true)
+    self.middle_name = '_' if (!self.middle_name.blank? && self.middle_name.length > 100 rescue true)
+    self.last_name = '_' if (!self.last_name.blank? && self.last_name.length > 100 rescue true)
   end
   def check_special_chars
       special = "?<>?[]}{=)(*&^%$#`~{}"
       regex = /[#{special.gsub(/./){|char| "\\#{char}"}}]/
-      self.first_name = "@@@@@" if (self.first_name =~ regex).to_i > 0
-      self.last_name = "@@@@@" if (self.last_name =~ regex).to_i > 0
-      self.middle_name = "@@@@@" if (self.middle_name =~ regex).to_i > 0
+      self.first_name = "_" if (self.first_name =~ regex).to_i > 0
+      self.last_name = "_" if (self.last_name =~ regex).to_i > 0
+      self.middle_name = "_" if (self.middle_name =~ regex).to_i > 0
   end
 end
