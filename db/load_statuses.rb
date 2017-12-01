775e24f3a185d262f3fc1599377303e3d6fbdcae
phundi <kennethkapundi1@gmail.com>
2017-12-01 15:57:01 +0200

    Record Dispatch List Crash Fix

--- db/load_statuses.rb
+++ db/load_statuses.rb
@@ -3,7 +3,7 @@ names = []
 
 CSV.foreach("#{Rails.root}/app/assets/data/person_status.csv", :headers => true) do |row|
  next if row[3].blank?
-# next if names.include?(row[3].squish)
+ next if names.include?(row[3].squish)
  names << row[3].squish
  
  status_type = Status.create!(name: row[3].squish, description: row[4].squish)
