bb5a1929c764ab84aea0d357b51366c01474376c
phundi <kennethkapundi1@gmail.com>
2017-12-19 18:12:27 +0200

    rescue blank description

--- db/load_notification_types.rb
+++ db/load_notification_types.rb
@@ -4,7 +4,7 @@ CSV.foreach("#{Rails.root}/app/assets/data/notification_types.csv", :headers => 
 
  NotificationType.create!(
     name: row[1].squish,
-    description: row[2].squish,
+    description: (row[2].squish rescue ""),
 	  level: row[0],
     trigger_status_id: Status.where(:name => row[4]).first.id,
     role_id: Role.where(role: row[3],level: row[0]).first.id)
