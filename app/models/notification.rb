e979cedfeabdc3081a74ae548c88e54f5b405768
phundi <kennethkapundi1@gmail.com>
2017-12-20 10:51:18 +0200

    Added pretty date module

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -6,7 +6,7 @@ class Notification < ActiveRecord::Base
 
   def self.by_role(role_id)
     data = Notification.find_by_sql("
-      SELECT n.notification_id, nt.name, nt.description, nt.role_id, prs.person_id, prs.status_id, prs.comments, s.name status FROM notification n
+      SELECT n.notification_id, nt.name, nt.description, nt.role_id, prs.person_id, prs.status_id, prs.comments, s.name status, s.created_at FROM notification n
         INNER JOIN notification_types nt
           ON nt.notification_type_id = n.notification_type_id
         INNER JOIN person_record_statuses prs
