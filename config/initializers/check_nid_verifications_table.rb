0fed4c467d6178ef1a439f79c237a368cc0ffe54
phundi <kennethkapundi1@gmail.com>
2018-09-19 14:14:28 +0200

    Added way of creating new table

--- config/initializers/check_nid_verifications_table.rb
+++ config/initializers/check_nid_verifications_table.rb
@@ -9,3 +9,15 @@ ActiveRecord::Base.connection.execute <<EOF
       PRIMARY KEY (`id`),
       UNIQUE INDEX `id_UNIQUE` (`id` ASC));
 EOF
+
+ActiveRecord::Base.connection.execute <<EOF
+    CREATE TABLE IF NOT EXISTS `error_records` (
+      `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+      `person_id` BIGINT(20) NOT NULL,
+      `passed` SMALLINT(6) NOT NULL,
+      `table_name`   VARCHAR(255),
+      `data`   TEXT,
+      `created_at`  TIMESTAMP NOT NULL,
+      PRIMARY KEY (`id`),
+      UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+EOF
