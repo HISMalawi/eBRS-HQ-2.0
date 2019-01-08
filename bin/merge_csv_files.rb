bdf4aa5b23a4485828cd2e0478224ab2a2ef309c
Kenneth Kapundi <kennethkapundi1@gmail.com>
2019-01-08 11:49:11 +0200

    Script to merge CSV files

---
+++ bin/merge_csv_files.rb
@@ -0,0 +1,23 @@
+path = ARGV[0]
+
+=begin
+  This method is used to combine a list of CSV files generated from
+  eBRS MYSQL using script bin/data_extract.rb
+=end
+
+def merge(list, path)
+
+  list.each do |file_name|
+
+    `head -1 #{path}/#{file_name} > merge.csv` if !File.exist?("merge.csv")
+    `sed  1d #{path}/#{file_name} >> temp.csv`
+    `sed '/^$/d' temp.csv >> merge.csv`
+    `rm temp.csv`
+  end
+end
+
+files = Dir.entries(path).delete_if{|f| !f.match("csv")}
+puts files
+puts "#{files.length} Files to be merged"
+
+merge(files, path)
