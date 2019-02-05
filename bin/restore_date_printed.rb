6215820c22ea187d769bab4e3c96807461a4a11d
Kenneth Kapundi <kennethkapundi1@gmail.com>
2019-02-05 16:33:32 +0200

    Finetuned script

--- bin/restore_date_printed.rb
+++ bin/restore_date_printed.rb
@@ -3,28 +3,27 @@
 #Written by: Kenneth Kapundi
 #Date: 05 February, 2019
 
-print_statuses = Status.where(" name IN ('DC-PRINTED', HQ-PRINTED', 'HQ-RE-PRINTED') ").map(&:status_id)
+print_statuses = Status.where(" name IN ('DC-PRINTED', 'HQ-PRINTED') ").map(&:status_id)
 dispatch_statuses = Status.where(" name IN ('HQ-DISPATCHED') ").map(&:status_id)
 
 births = PersonBirthDetail.where(" district_id_number IS NOT NULL ")
 count = births.count
 births.each_with_index do |birth, i|
 
-  puts "#{i}/#{count}"
   date = nil
   if !birth.source_id.blank? && birth.source_id.length > 19
     #Migrated From Old System
     remote_data = JSON.parse(`curl --silent http://192.168.48.2:5900/ebrs_hq/#{birth.source_id}`) rescue (puts "Missing for #{birth.source_id}")
     date        = remote_data['printed_at'].to_datetime rescue nil
     date_issued = remote_data['date_certificate_issued'].to_datetime rescue nil
-    puts "R--#{date}##{date_issued}"
+    puts "R--##{date}##{date_issued}##{i}/#{count}"
   elsif birth.source_id.blank? || birth.source_id.length <= 19
     local_date = PersonRecordStatus.where(" status_id IN (#{print_statuses.join(',')}) ").first
     local_issue_date = PersonRecordStatus.where(" status_id IN (#{dispatch_statuses.join(',')}) ").first
 
     date          = local_date.created_at.to_datetime rescue nil
     date_issued   = local_issue_date.to_datetime rescue nil
-    puts "L--#{date}##{date_issued}"
+    puts "L--##{date}##{date_issued}##{i}/#{count}"
   end
 
   if !date.blank?
