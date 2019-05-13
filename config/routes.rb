69c981613bc7e33c1adc3e7967a8a27913c971a4
Kenneth Kapundi <kennethkapundi1@gmail.com>
2019-05-13 13:54:53 +0200

    Changed route

--- config/routes.rb
+++ config/routes.rb
@@ -189,7 +189,7 @@ Rails.application.routes.draw do
   get '/ajax_assign_barcode'  => 'person#ajax_assign_barcode'
 
   get '/nid_queue_count' => 'person#nid_queue_count'
-  get '/remote_nid_request' => 'person#nid_remote_count'
+  get '/person/remote_nid_request'
 
 
   ########################### (create record form) routes
