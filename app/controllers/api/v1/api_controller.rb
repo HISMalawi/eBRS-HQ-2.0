fdbcfea654a9885f4fb4368d69526785781f7f89
Jacob Mziya <jacobmziya@gmail.com>
2020-07-27 15:37:54 +0200

    Created api branch.

---
+++ app/controllers/api/v1/api_controller.rb
@@ -0,0 +1,31 @@
+module Api
+  module V1
+    class ApiController < ApplicationController
+      before_action :check_basic_auth
+      skip_before_action :verify_authenticity_token
+
+      private
+
+      def check_basic_auth
+        unless request.authorization.present?
+          head :unauthorized
+          return
+        end
+
+        authenticate_with_http_basic do |username, password|
+          user = User.get_active_user(username)
+          if user && user.password_matches?(password)
+            login! user
+            @current_user = user
+          else
+            head :unauthorized
+          end
+        end
+      end
+
+      def current_user
+        @current_user
+      end
+    end
+  end
+end
---
+++ app/controllers/api/v1/duplicates_controller.rb
@@ -0,0 +1,23 @@
+module Api
+  module V1
+    class DuplicatesController < ApiController
+      # GET /births
+      def index
+
+      end
+
+      # GET /births/:id
+      def show; end
+
+      # POST /births
+      def create; end
+
+      # PUT /births/:id
+      def update; end
+
+      # DELETE /births/:id
+      def destroy; end
+
+    end
+  end
+end
