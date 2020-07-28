6bbf0463736b6c404abc4252951e121d8c4a45d2
Jacob Mziya <jacobmziya@gmail.com>
2020-07-28 11:05:34 +0200

    Created resources for births.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   ####################### API ####################################
   namespace :api do
     namespace :v1 do
+      resources :births
       resources :duplicates
       resources :certificates
     end
