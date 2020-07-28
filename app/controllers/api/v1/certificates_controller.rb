c619d4429ea0a9b0da6e83e85b302dd54b2a1682
Jacob Mziya <jacobmziya@gmail.com>
2020-07-28 11:01:41 +0200

    Created status endpoint parameters for certificates.

--- app/controllers/api/v1/certificates_controller.rb
+++ app/controllers/api/v1/certificates_controller.rb
@@ -5,7 +5,19 @@ module Api
       def index
         certificates_count = Certificate.count
 
-        render json: {'Total Certificates' => certificates_count}
+        response = case params[:status]
+                   when 'printed'
+                     printed = Report.printed('2019-01-01','2019-01-29',[])
+                     { 'Certificates printed' => printed }
+                   when 'dispatched'
+                     dispatched = Report.dispatched('2019-01-01','2019-01-29',[])
+                     { 'Certificates dispatched' => dispatched }
+                   else
+                     # type code here
+                     { 'Total certificates' => certificates_count}
+                   end
+
+        render :json => response
       end
 
       # GET /certificates/:id
