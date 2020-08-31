65ae10aae1930232e0abf7b6cdb404ab932169c2
Jacob Mziya <jacobmziya@gmail.com>
2020-08-31 13:48:23 +0200

    Used base connection instead of activerecord.

--- app/controllers/api/v1/births_controller.rb
+++ app/controllers/api/v1/births_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::BirthsController < Api::V1::ApiController
   # GET /births
   def index
+    # TODO: Make dynamic once filter feature is added in CRVS Visio
+    start_date = '01-01-2019'
+    end_date = '20-01-2019'
     #   status_ids = Status.where(" name = 'HQ-CAN-PRINT' ").map(&:status_id)
     #
     #   PersonBirthDetail.find_by_sql(" SELECT * FROM person_birth_details d
@@ -10,9 +13,9 @@ class Api::V1::BirthsController < Api::V1::ApiController
     #   GROUP BY d.person_id").count
 
     response = {
-        'total' => PersonBirthDetail.count,
-        'registered' => PersonBirthDetail.count,
-        'approved' => PersonBirthDetail.count
+        'reported' => PersonBirthDetail.count,
+        'registered' => PersonBirthDetail.where.not(date_registered: [nil,'']).count,
+        'approved' => approved
     }
 
     render :json => response
@@ -29,5 +32,21 @@ class Api::V1::BirthsController < Api::V1::ApiController
 
   # DELETE /births/:id
   def destroy; end
+
+  private
+
+  def reported
+    query = ""
+  end
+  def approved
+
+    query = "SELECT count(*) as total FROM person_birth_details d
+    INNER JOIN person_record_statuses prs ON prs.person_id = d.person_id
+    WHERE (d.source_id IS NULL OR LENGTH(d.source_id) >  19)
+    AND prs.status_id IN (select status_id FROM statuses where name = 'HQ-CAN-PRINT');"
+
+    ActiveRecord::Base.connection.select_all(query).as_json.last['total'] rescue 0
+
+  end
 
 end
