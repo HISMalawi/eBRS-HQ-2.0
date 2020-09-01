c96261bb39ba273ac5867ec652fc9be263f95ba3
Jacob Mziya <jacobmziya@gmail.com>
2020-09-01 12:16:11 +0200

    Optimizing registered query method. 60%

--- app/controllers/api/v1/birth_reports_controller.rb
+++ app/controllers/api/v1/birth_reports_controller.rb
@@ -24,12 +24,12 @@ class Api::V1::BirthReportsController < Api::V1::ApiController
       results[district] = {}
       results[district]["facility_registered"]  = registered(start_date, end_date, facilities)
       results[district]["dro_registered"] = registered(start_date, end_date, [district_id])
-      # results[district]["printed"]  = Report.printed(start_date, end_date, all_district_locs)
+      results[district]["printed"]  = printed(start_date, end_date, all_district_locs)
       results[district]["total_registered"]  = registered(start_date, end_date, all_district_locs)
       #
       results[district]["cum_facility_registered"]  = registered("01-01-2000".to_date, end_date, facilities)
       results[district]["cum_dro_registered"] = registered("01-01-2000".to_date, end_date, [district_id])
-      # results[district]["cum_printed"]  = Report.printed("01-01-2000".to_date, end_date, all_district_locs)
+      results[district]["cum_printed"]  = printed("01-01-2000".to_date, end_date, all_district_locs)
       results[district]["cum_total_registered"]  = registered("01-01-2000".to_date, end_date, all_district_locs)
       #
       # results[district]["registered_but_born_in_hospital"]  = Report.by_place_of_birth(start_date, end_date, "Hospital", all_district_locs)
@@ -48,8 +48,31 @@ class Api::V1::BirthReportsController < Api::V1::ApiController
   private
 
   def registered(start_date, end_date, location_ids=[])
-    PersonBirthDetail.where(" DATE(date_registered) BETWEEN '#{start_date.to_date.to_s}' AND '#{end_date.to_date.to_s}'
-      AND (source_id IS NULL OR LENGTH(source_id) >  19) AND location_created_at IN (#{location_ids.join(', ')}) ").count
+    loc_query = ""
+    unless location_ids.blank?
+      loc_query = " AND location_created_at IN (#{location_ids.join(', ')}) "
+    end
+
+    query = "SELECT count(*) as total FROM person_birth_details
+WHERE date_registered BETWEEN '#{start_date.to_date.to_s}'
+AND '#{end_date.to_date.to_s}' AND (source_id IS NULL OR LENGTH(source_id) >  19) #{loc_query};"
+
+    ActiveRecord::Base.connection.select_all(query).as_json.last['total'] rescue 0
+  end
+
+  def printed(start_date, end_date, location_ids=[])
+    loc_query = ""
+    unless location_ids.blank?
+      loc_query = " AND location_created_at IN (#{location_ids.join(', ')}) "
+    end
+
+    query = "SELECT count(*) as total FROM person_birth_details d
+    INNER JOIN person_record_statuses prs ON prs.person_id = d.person_id
+    WHERE DATE(prs.created_at) BETWEEN '#{start_date.to_date.to_s}' AND '#{end_date.to_date.to_s}'
+    AND (d.source_id IS NULL OR LENGTH(d.source_id) >  19)
+    AND prs.status_id IN (select status_id from statuses where name IN ('DC-PRINTED', 'HQ-PRINTED', 'HQ-DISPATCHED'))"
+
+    ActiveRecord::Base.connection.select_all(query).as_json.last['total'] rescue 0
   end
 
 end
--- app/controllers/api/v1/births_controller.rb
+++ app/controllers/api/v1/births_controller.rb
@@ -13,7 +13,7 @@ class Api::V1::BirthsController < Api::V1::ApiController
     #   GROUP BY d.person_id").count
 
     response = {
-        'reported' => PersonBirthDetail.count,
+        'reported' => reported,
         'registered' => PersonBirthDetail.where.not(date_registered: [nil,'']).count,
         'approved' => approved
     }
@@ -36,17 +36,24 @@ class Api::V1::BirthsController < Api::V1::ApiController
   private
 
   def reported
-    query = ""
-  end
-  def approved
+    query = "select count(*) as total from person_birth_details;"
 
+    ActiveRecord::Base.connection.select_all(query).as_json.last['total'] rescue 0
+  end
+
+  def registered
+    query = "SELECT count(*) as total FROM person_birth_details WHERE date_registered IS NOT NULL"
+
+    ActiveRecord::Base.connection.select_all(query).as_json.last['total'] rescue 0
+  end
+
+  def approved
     query = "SELECT count(*) as total FROM person_birth_details d
     INNER JOIN person_record_statuses prs ON prs.person_id = d.person_id
     WHERE (d.source_id IS NULL OR LENGTH(d.source_id) >  19)
     AND prs.status_id IN (select status_id FROM statuses where name = 'HQ-CAN-PRINT');"
 
     ActiveRecord::Base.connection.select_all(query).as_json.last['total'] rescue 0
-
   end
 
 end
